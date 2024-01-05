937365baae45efb7ca02baaca1b326509f3c6f62
LucasAdao <l.adao@outlook.com>
2024-01-05 19:21:41 -0300

    Função app de music e ligacao totalmente bufada

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Celulares_tipos.models;
 
 namespace Celulares_tipos
 {
-    public class Iphone
+    public class Program
     {
-        
+        static void Main() 
+        {
+            
+        }
     }
 }
--- models/Nokia.cs
+++ models/Nokia.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Celulares_tipos.util;
@@ -35,7 +36,8 @@ namespace Celulares_tipos.models
                         string autor = Console.ReadLine();
                         Musicas music = new Musicas(nomeDaMusica, autor);
                         ListaDeMusicas.Add(music);
-                        
+                        Console.WriteLine("           ");
+                        Thread.Sleep(1000);
                         break;
                     case 2:
                         Console.WriteLine("Digite o nome da música que você deseja remover:");
@@ -44,13 +46,21 @@ namespace Celulares_tipos.models
                         string autorParaRemover = Console.ReadLine();
                         Musicas musicQueSeraRemovida = new Musicas(nomeDaMusicaParaRemover, autorParaRemover);
                         ListaDeMusicas.Remove(musicQueSeraRemovida);
+                        Console.WriteLine("           ");
+                        Thread.Sleep(1000);
                         break;
                     case 3:
-                        
-
+                        foreach (Musicas musica in ListaDeMusicas)
+                        {
+                            Console.WriteLine(musica.NomeDaMusica + " - " + musica.Autor);
+                            Thread.Sleep(1000);
+                        }
+                        Console.WriteLine("           ");
+                        break;
+                    case 4:
+                        musicPlayer = false;
                         break;
                         
-
                     default:
                         Console.WriteLine("Digite uma opção válida.");
                         break;
@@ -62,20 +72,36 @@ namespace Celulares_tipos.models
         
         public override void FazerLigacao()
         {
-            //TO DO: implementar contato que vc quer ligar
-            
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("Digite o número que você deseja fazer a ligação:");
+            string numeroLigacao = Console.ReadLine();
             Random random = new Random();
-            int procedimentosDeLigacao = random.Next(1, 2);
+            int procedimentosDeLigacao = random.Next(1, 3);
             if (procedimentosDeLigacao == 1)
             {
                 Console.WriteLine("Chamando...");
                 Thread.Sleep(1000);
                 Console.WriteLine("Chamando...");
                 Thread.Sleep(2000);
-                Console.WriteLine("Chamada em Andamento");
+                Console.WriteLine("Chamada em Andamento com "+ numeroLigacao);
                 Console.WriteLine("  ");
+                stopwatch.Start();
+
                 Console.WriteLine("Para Desligar digite uma tecla");
                 Console.ReadKey();
+                stopwatch.Stop();
+                TimeSpan tempoDecorrido = stopwatch.Elapsed;
+                string tempoMinutos = tempoDecorrido.TotalMinutes.ToString("F2");
+                string tempoSegundos = tempoDecorrido.TotalSeconds.ToString("F2");
+                if (tempoDecorrido.TotalMinutes < 1)
+                {
+                    Console.WriteLine($"Chamada encerrada! você ficou na chamada {tempoSegundos} ");
+                }
+                else 
+                {
+                    Console.WriteLine($"Chamada encerrada! você ficou na chamada {tempoMinutos}");
+                }
             }
             else
             { 
@@ -83,7 +109,7 @@ namespace Celulares_tipos.models
                 Thread.Sleep(1000);
                 Console.WriteLine("Chamando...");
                 Thread.Sleep(2000);
-                Console.WriteLine("O celular está fora de área ou desligado!");
+                Console.WriteLine("O número: " + numeroLigacao + " está fora de área ou desligado!");
             }
 
         }
